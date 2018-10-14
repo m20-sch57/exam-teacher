58c08f2707b12b474f82c22ed16e1b5ae9e72011
Feodor Kuyanov <feodor.kuyanov@gmail.com>
2018-10-14 19:22:43 +0300

    Some changes

--- Server.py
+++ Server.py
@@ -2,6 +2,19 @@ from xmlrpc.server import SimpleXMLRPCServer
 import os
 
 
+class Item:
+    def __init__(self, path):
+        self.path = path
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def set_attr(self, attr, value):
+        open(os.path.join(self.path, attr), 'w', encoding=ENCODING).write(str(value))
+
+    def get_attr(self, attr):
+        return open(os.path.join(self.path, attr), encoding=ENCODING).read()
+
+
 def format_str(s):
     s = s.lower()
     s = s.replace('ё', 'е')
@@ -35,33 +48,51 @@ def number_of_questions(group_name, exam_name):
     return len(os.listdir(os.path.join('groups', group_name, 'exams', exam_name)))
 
 
+def first_not_passed_question(group_name, user_name, exam_name):
+    directory = os.path.join('groups', group_name, 'users', user_name, exam_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for question_number in range(1, number_of_questions(group_name, exam_name) + 1):
+        if not os.path.exists(os.path.join(directory, str(question_number))):
+            return question_number
+    return number_of_questions(group_name, exam_name) + 1
+
+
 def get_question(group_name, exam_name, question_number):
-    directory = os.path.join('groups', group_name, 'exams', exam_name, str(question_number))
-    question_type = open(os.path.join(directory, 'type'), encoding=encoding).read()
-    if question_type == 'Тест':
-        return {'statement': open(os.path.join(directory, 'statement'), encoding=encoding).read(),
-                'variants': open(os.path.join(directory, 'variants'), encoding=encoding).read().split('\n'),
-                'time': int(open(os.path.join(directory, 'time'), encoding=encoding).read()),
-                'type': question_type}
-    elif question_type == 'Короткий ответ':
-        return {'statement': open(os.path.join(directory, 'statement'), encoding=encoding).read(),
-                'time': int(open(os.path.join(directory, 'time'), encoding=encoding).read()),
-                'type': question_type}
+    question_item = Item(os.path.join('groups', group_name, 'exams', exam_name, str(question_number)))
+    if question_item.get_attr('type') == 'Тест':
+        return {'statement': question_item.get_attr('statement'),
+                'variants': question_item.get_attr('variants').split('\n'),
+                'correct': int(question_item.get_attr('correct')),
+                'time': int(question_item.get_attr('time')),
+                'type': question_item.get_attr('type'),
+                'max': 1}
+    elif question_item.get_attr('type') == 'Короткий ответ':
+        return {'statement': question_item.get_attr('statement'),
+                'correct': question_item.get_attr('correct').split('\n'),
+                'time': int(question_item.get_attr('time')),
+                'type': question_item.get_attr('type'),
+                'max': 1}
     else:
         return {}
 
 
-def get_details(group_name, exam_name, question_number):
-    directory = os.path.join('groups', group_name, 'exams', exam_name, str(question_number))
-    question_type = open(os.path.join(directory, 'type'), encoding=encoding).read()
-    if question_type == 'Тест':
-        return {'correct': int(open(os.path.join(directory, 'correct'), encoding=encoding).read())}
-    elif question_type == 'Короткий ответ':
-        return {'correct': open(os.path.join(directory, 'correct'), encoding=encoding).read().split('\n')}
-    elif question_type == 'Развёрнутый ответ':
-        return {}
-    else:
-        return {}
+def get_answer(group_name, user_name, exam_name, question_number):
+    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
+    return {'answer': answer_item.get_attr('answer'),
+            'score': int(answer_item.get_attr('score'))}
+
+
+def view_question(group_name, user_name, exam_name, question_number):
+    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
+    answer_item.set_attr('answer', '')
+    answer_item.set_attr('score', 0)
+    return get_question(group_name, exam_name, question_number)
+
+
+def view_details(group_name, user_name, exam_name, question_number):
+    return {**get_question(group_name, exam_name, question_number),
+            **get_answer(group_name, user_name, exam_name, question_number)}
 
 
 def test_checker(answer, correct):
@@ -75,28 +106,30 @@ def short_checker(answer, correct):
 
 def check(group_name, user_name, exam_name, question_number, answer):
     question = get_question(group_name, exam_name, question_number)
-    details = get_details(group_name, exam_name, question_number)
+    reply = {}
     if question['type'] == 'Тест':
-        return {'score': test_checker(answer, details['correct']),
-                'maximum': 1}
+        reply = {'score': test_checker(answer, question['correct'])}
     elif question['type'] == 'Короткий ответ':
-        return {'score': short_checker(answer, details['correct']),
-                'maximum': 1}
+        reply = {'score': short_checker(answer, question['correct'])}
     elif question['type'] == 'Развёрнутый ответ':
-        return {'score': -1,
-                'maximum': details['maximum']}
-    else:
-        return {}
+        reply = {'score': -1}
+    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
+    answer_item.set_attr('answer', answer)
+    answer_item.set_attr('score', reply['score'])
+    return reply
 
 
-encoding = 'utf-8-sig'
+ENCODING = 'utf-8-sig'
 server = SimpleXMLRPCServer(('', 8000))
 server.register_function(check_connection)
 server.register_function(search_group)
 server.register_function(search_user)
 server.register_function(list_of_exams)
 server.register_function(number_of_questions)
+server.register_function(first_not_passed_question)
 server.register_function(get_question)
-server.register_function(get_details)
+server.register_function(get_answer)
+server.register_function(view_question)
+server.register_function(view_details)
 server.register_function(check)
 server.serve_forever()
