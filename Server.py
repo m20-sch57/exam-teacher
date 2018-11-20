a0a7e6b5f8ac40776005e6a70edb58e497f85212
Feodor Kuyanov <feodor.kuyanov@gmail.com>
2018-11-20 20:15:58 +0300

    New features

--- Server.py
+++ Server.py
@@ -1,10 +1,16 @@
-from xmlrpc.server import SimpleXMLRPCServer
+"""
+Examiner project, server.
+Version 1.0
+"""
+
 import os
+import time
+from xmlrpc.server import SimpleXMLRPCServer
 
 
 class Item:
     """
-    Item with fields that should be saved on disk
+    Item with attributes that should be saved on disk.
     """
     def __init__(self, path):
         self.path = path
@@ -13,175 +19,161 @@ class Item:
 
     def set_attr(self, attr, value):
         """
-        Sets value of attribute
+        Sets value of the attribute.
         """
         open(os.path.join(self.path, attr), 'w', encoding=ENCODING).write(str(value))
 
     def get_attr(self, attr):
         """
-        Gets value of attribute
+        Gets value of the attribute.
         """
+        if not os.path.isfile(os.path.join(self.path, attr)):
+            return False
         return open(os.path.join(self.path, attr), encoding=ENCODING).read()
 
 
 def format_str(string):
     """
-    Formats string: lowers letters and removes all other symbols except numbers
+    Formats string: lowers letters and removes all other symbols except numbers.
     """
-    string = string.lower()
-    string = string.replace('ё', 'е')
-    string = ''.join([c for c in string if c.isalnum()])
+    # string = string.lower()
+    # string = string.replace('ё', 'е')
+    # string = ''.join([c for c in string if c.isalnum()])
+    # TODO: configure!
     return string
 
 
-def check_connection():
+def login(group, user, password):
     """
-    Verifying connection
+    Checks if the user is valid.
     """
+    if not group in os.listdir('groups'):
+        return False
+    if not user in os.listdir(os.path.join('groups', group, 'users')):
+        return False
+    # TODO: add password verification
     return True
 
 
-def search_group(group_name):
+def list_of_exams(group):
     """
-    Searches for group, using format_str, and returns the name of the group
+    Returns list of all available exams in the group.
     """
-    for cur_group in os.listdir('groups'):
-        if format_str(cur_group) == format_str(group_name):
-            return cur_group
-    return ''
+    return os.listdir(os.path.join('groups', group, 'exams'))
 
 
-def search_user(group_name, user_name):
+def get_question(group, user, exam, question):
     """
-    Searches for user, using format_str, and returns the user name
+    Returns the question data.
     """
-    for cur_user in os.listdir(os.path.join('groups', group_name, 'users')):
-        if format_str(cur_user) == format_str(user_name):
-            return cur_user
-    return ''
+    question_item = Item(os.path.join('groups', group, 'exams', exam, str(question)))
+    answer_item = Item(os.path.join('groups', group, 'users', user, exam, str(question)))
+    return {'type': question_item.get_attr('type'),
+            'statement': question_item.get_attr('statement'),
+            'correct': question_item.get_attr('correct'),
+            'maxscore': question_item.get_attr('maxscore'),
+            'answer': answer_item.get_attr('answer'),
+            'score': answer_item.get_attr('score')}
 
 
-def list_of_exams(group_name):
+def get_exam(group, user, exam):
     """
-    Returns list of all available exams in the group
+    Returns all question data in the exam.
     """
-    return os.listdir(os.path.join('groups', group_name, 'exams'))
+    quantity = len(os.listdir(os.path.join('groups', group, 'exams', exam))) - 1
+    exam_data = [get_question(group, user, exam, question) for question in range(1, quantity + 1)]
+    return exam_data
 
 
-def number_of_questions(group_name, exam_name):
+def get_exam_info(group, user, exam):
     """
-    Returns number of questions in exam
+    Returns exam's info: start time, duration time, total student's score, total maximum score.
     """
-    return len(os.listdir(os.path.join('groups', group_name, 'exams', exam_name)))
-
-
-def first_not_passed_question(group_name, user_name, exam_name):
-    """
-    Finds the first not seen question
-    """
-    directory = os.path.join('groups', group_name, 'users', user_name, exam_name)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    for question_number in range(1, number_of_questions(group_name, exam_name) + 1):
-        if not os.path.exists(os.path.join(directory, str(question_number))):
-            return question_number
-    return number_of_questions(group_name, exam_name) + 1
-
-
-def get_question(group_name, exam_name, question_number):
-    """
-    Returns the question data without student's answer
-    """
-    question_item = Item(os.path.join('groups', group_name, 'exams', exam_name, str(question_number)))
-    if question_item.get_attr('type') == 'Тест':
-        variants = question_item.get_attr('variants').split('\n')
-        return {'statement': question_item.get_attr('statement'),
-                'variants': variants,
-                'correct': variants[int(question_item.get_attr('correct')) - 1],
-                'time': int(question_item.get_attr('time')),
-                'type': question_item.get_attr('type'),
-                'max': 1}
-    elif question_item.get_attr('type') == 'Короткий ответ':
-        return {'statement': question_item.get_attr('statement'),
-                'correct': question_item.get_attr('correct').split('\n'),
-                'time': int(question_item.get_attr('time')),
-                'type': question_item.get_attr('type'),
-                'max': 1}
+    exam_item = Item(os.path.join('groups', group, 'exams', exam, 'settings'))
+    user_item = Item(os.path.join('groups', group, 'users', user, exam, 'settings'))
+    exam_data = get_exam(group, user, exam)
+    quantity = len(os.listdir(os.path.join('groups', group, 'exams', exam))) - 1
+    if user_item.get_attr('start') is False:
+        state = 'Not started'
+    elif int(time.time()) < int(user_item.get_attr('end')):
+        state = 'Running'
     else:
-        return {}
+        state = 'Finished'
+    total_score = 0
+    total_maxscore = 0
+    for question in range(1, len(exam_data) + 1):
+        question_data = exam_data[question - 1]
+        if question_data['score'] is not False:
+            total_score += int(question_data['score'])
+        if question_data['maxscore'] is not False:
+            total_maxscore += int(question_data['maxscore'])
+    return {'state': state,
+            'duration': exam_item.get_attr('duration'),
+            'start': user_item.get_attr('start'),
+            'end': user_item.get_attr('end'),
+            'quantity': quantity,
+            'total_score': total_score,
+            'total_maxscore': total_maxscore}
 
 
-def get_answer(group_name, user_name, exam_name, question_number):
+def start_exam(group, user, exam):
     """
-    Returns the student's answer
+    Starts the exam.
     """
-    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
-    return {'answer': answer_item.get_attr('answer'),
-            'score': int(answer_item.get_attr('score'))}
+    exam_item = Item(os.path.join('groups', group, 'exams', exam, 'settings'))
+    user_item = Item(os.path.join('groups', group, 'users', user, exam, 'settings'))
+    current_time = int(time.time())
+    duration_time = int(exam_item.get_attr('duration')) * 60
+    user_item.set_attr('start', current_time)
+    user_item.set_attr('end', current_time + duration_time)
+    return True
 
 
-def view_question(group_name, user_name, exam_name, question_number):
+def finish_exam(group, user, exam):
     """
-    Registers that student opened question
+    Finishes the exam.
     """
-    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
-    answer_item.set_attr('answer', '')
-    answer_item.set_attr('score', 0)
-    return get_question(group_name, exam_name, question_number)
+    user_item = Item(os.path.join('groups', group, 'users', user, exam, 'settings'))
+    current_time = int(time.time())
+    user_item.set_attr('end', current_time)
+    return True
 
 
-def view_details(group_name, user_name, exam_name, question_number):
+def save_answer(group, user, exam, question, answer):
     """
-    Returns all question info including the student's answer
+    Saves student's answer.
     """
-    return {**get_question(group_name, exam_name, question_number),
-            **get_answer(group_name, user_name, exam_name, question_number)}
-
-
-def test_checker(answer, correct):
-    """
-    Checker for Test question
-    """
-    return int(answer == correct)
-
-
-def short_checker(answer, correct):
-    """
-    Checker for Short question
-    """
-    answer = format_str(answer)
-    return int(format_str(answer) in list(map(format_str, correct)))
-
-
-def check(group_name, user_name, exam_name, question_number, answer):
-    """
-    Checks student's answer and saves it on disk
-    """
-    question = get_question(group_name, exam_name, question_number)
-    reply = {}
-    if question['type'] == 'Тест':
-        reply = {'score': test_checker(answer, question['correct'])}
-    elif question['type'] == 'Короткий ответ':
-        reply = {'score': short_checker(answer, question['correct'])}
-    elif question['type'] == 'Развёрнутый ответ':
-        reply = {'score': -1}
-    answer_item = Item(os.path.join('groups', group_name, 'users', user_name, exam_name, str(question_number)))
+    answer_item = Item(os.path.join('groups', group, 'users', user, exam, str(question)))
     answer_item.set_attr('answer', answer)
-    answer_item.set_attr('score', reply['score'])
-    return reply
+    return True
+
+
+def check(group, user, exam, question):
+    """
+    Checks student's answer to the short question.
+    """
+    question_item = Item(os.path.join('groups', group, 'exams', exam, str(question)))
+    answer_item = Item(os.path.join('groups', group, 'users', user, exam, str(question)))
+    if question_item.get_attr('type') == 'Short':
+        answer = answer_item.get_attr('answer')
+        correct = question_item.get_attr('correct')
+        if format_str(answer) == format_str(correct):
+            answer_item.set_attr('score', 1)
+        else:
+            answer_item.set_attr('score', 0)
+    # TODO: add more types!
+    return True
 
 
 ENCODING = 'utf-8-sig'
-server = SimpleXMLRPCServer(('', 8000))
-server.register_function(check_connection)
-server.register_function(search_group)
-server.register_function(search_user)
-server.register_function(list_of_exams)
-server.register_function(number_of_questions)
-server.register_function(first_not_passed_question)
-server.register_function(get_question)
-server.register_function(get_answer)
-server.register_function(view_question)
-server.register_function(view_details)
-server.register_function(check)
-server.serve_forever()
+SERVER = SimpleXMLRPCServer(('', 8000))
+SERVER.register_function(login)
+SERVER.register_function(list_of_exams)
+SERVER.register_function(get_question)
+SERVER.register_function(get_exam)
+SERVER.register_function(get_exam_info)
+SERVER.register_function(start_exam)
+SERVER.register_function(finish_exam)
+SERVER.register_function(save_answer)
+SERVER.register_function(check)
+SERVER.serve_forever()
